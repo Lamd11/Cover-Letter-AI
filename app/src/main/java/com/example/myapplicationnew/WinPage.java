691a649623665a5af00f900ef0787ae0d2b403b0
Mathew-Y <mathewyounan@gmail.com>
2022-04-20 18:29:55 -0400

    Beginning WIP of minor improvements

--- app/src/main/java/com/example/myapplicationnew/WinPage.java
+++ app/src/main/java/com/example/myapplicationnew/WinPage.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import nl.dionsegijn.konfetti.xml.KonfettiView;
 
 public class WinPage extends AppCompatActivity {
 
